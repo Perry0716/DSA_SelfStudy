f37c3fb4db0f2300b55798fbf86d4c801bdc9acd
Perry0716 <perrydash6@gmail.com>
2024-05-16 21:40:32 +0530

    List reverse

--- DSA-logic/LinkedLists/z_01_LinkedLists.java
+++ DSA-logic/LinkedLists/z_01_LinkedLists.java
@@ -35,8 +35,31 @@ public class z_01_LinkedLists {
                 tail = temp;
 
             }
-
             size++;
+        }
+
+        public void addAtIndex(int idx, int val){
+            if(idx < 0 || idx > size){
+                System.out.println("Invalid");
+            }
+            else if(idx == 0) {
+                addListFirst(val);
+            }
+            else if(idx == size){
+                addLast(val);
+            }
+            else {
+                Node node = new Node();
+                node.data = val;
+
+                Node temp = head;
+            for(int i = 0; i <idx; i++){
+                temp = temp.next;
+            }
+            node.next = temp.next;
+            temp.next = node;
+            size++;
+            }
         }
 
         int removeFirst(){
@@ -58,6 +81,53 @@ public class z_01_LinkedLists {
                     size--;
                     return val;
                 }
+            }
+        }
+
+        int removeLast() {
+            if (size == 0 || size < 0) {
+                System.out.println("Invalid");
+                return -1;
+            } else if (size == 1) {
+                int val = tail.data;
+                head = tail = null;
+                size = 0;
+                return val;
+            } else {
+                int val = tail.data;
+                Node temp = head;
+                for (int i = 0; i < size - 2; i++) {
+                    temp = temp.next;
+                }
+                tail = temp;
+                temp.next = null;
+                size--;
+                return val;
+            }
+        }
+
+        int removeAt(int idx){
+            if (idx > size || idx < 0) {
+                System.out.println("Invalid");
+                return -1;
+            }
+            else if (idx == 0) {
+                int val = removeFirst();
+                return val;
+            }
+            else if(idx == size){
+                int val =removeLast();
+                return val;
+            }
+            else {
+                Node temp = head;
+                for (int i = 0; i < idx - 1; i++) {
+                    temp = temp.next;
+                }
+                temp.next = temp.next.next;
+                int val = temp.next.data;
+                size--;
+                return val;
             }
         }
 
@@ -105,6 +175,48 @@ public class z_01_LinkedLists {
             }
             System.out.println();
         }
+
+        void reverseWRTData(){          //uses getNode private method for getting a node
+            int li = 0;
+            int rt = size - 1;
+            while (li < rt){
+               Node left = getNode(li);
+               Node right = getNode(rt);
+
+               int temp = left.data;
+               left.data = right.data;
+               right.data = temp;
+
+               li++;
+               rt--;
+
+            }
+        }
+
+        private Node getNode(int idx){
+            Node temp = head;
+                for (int i=0; i<idx; i++) {
+                    temp = temp.next;
+                }
+                return temp;
+        }
+
+        void reverseWrtPointer(){
+            Node previous = null;
+            Node current = head;
+
+            while (current != null){
+                Node originalCurr = current.next;
+                current.next = previous;
+
+                previous = current;
+                current = originalCurr;
+            }
+
+            Node temp = head;
+            head = tail;
+            tail = temp;
+        }
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -115,11 +227,29 @@ public class z_01_LinkedLists {
             if(str.startsWith("addList")){
                 list.addLast(Integer.parseInt(str.split(" ")[1]));
             }
-            if(str.startsWith("addListFirst")){
+            else if(str.startsWith("addListFirst")){
                 list.addListFirst(Integer.parseInt(str.split(" ")[1]));
+            }
+            else if(str.startsWith("addListFirst")){
+                list.addListFirst(Integer.parseInt(str.split(" ")[1]));
+            }
+            else if(str.startsWith("addAtIndex")){
+                list.addAtIndex(Integer.parseInt(str.split(" ")[1]), Integer.parseInt(str.split(" ")[2]));
             }
             else if(str.startsWith("removeFirst")){
                 int val = list.removeFirst();
+                if(val != -1){
+                    System.out.println(val);
+                }
+            }
+            else if(str.startsWith("removeLast")){
+                int val = list.removeLast();
+                if(val != -1){
+                    System.out.println(val);
+                }
+            }
+            else if(str.startsWith("removeAt")){
+                int val = list.removeAt(Integer.parseInt(str.split(" ")[1]));
                 if(val != -1){
                     System.out.println(val);
                 }
@@ -145,6 +275,12 @@ public class z_01_LinkedLists {
             }
             else if(str.startsWith("size")){
                 System.out.println(list.size());
+            }
+            else if(str.startsWith("reverseLLData")){
+                list.reverseWRTData();
+            }
+            else if(str.startsWith("reverseLLpointer")){
+                list.reverseWrtPointer();
             }
 
         }
